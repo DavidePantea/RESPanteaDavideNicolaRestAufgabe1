12a77ee8013b8531533db37aec5d07445e00dbe6
DavidePantea <davide.pantea22@gmail.com>
2025-02-18 11:47:20 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,8 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         List<NinjaEvent> ninjaEvents = readXml("src/ninja_events.xml");
